287d7c6f5783d3be73d6790935011780e20a4abd
Juana Larrumbide <juanalarrumbide@gmail.com>
2024-01-19 11:44:49 -0500

    end

--- Proyecto/core/urls.py
+++ Proyecto/core/urls.py
@@ -1,16 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
-
-from django.urls import path 
-
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index, name="index"),
+    path("", views.index, name="index"),
     path("profesor/list/", views.profesor_list, name="profesor_list"),
     path("profesor/create/", views.profesor_create, name="profesor_create"),
     path("estudiantes/list/", views.estudiante_list, name="estudiante_list"),
     path("estudiantes/create/", views.estudiante_create, name="estudiante_create"),
+    path("cursos/", views.CursoListView.as_view(), name="curso_list"),
 ]
 
 if settings.DEBUG:
--- Proyecto/core/views.py
+++ Proyecto/core/views.py
@@ -1,38 +1,43 @@
-from django.shortcuts import redirect, render
-from . import models
-from . import forms 
-from . models import Profesor, Estudiante
 
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from .models import Profesor, Estudiante, Curso
+from .forms import ProfesorForm, EstudianteForm
 
 def index(request):
     return render(request, "core/index.html")
 
 def profesor_list(request):
-    consulta = models.Profesor.objects.all()
+    consulta = Profesor.objects.all()
     contexto = {"profesores": consulta}
     return render(request, "core/profesor_list.html", contexto) 
 
 def profesor_create(request):
     if request.method == "POST":
-        form = forms.ProfesorForm(request.POST)
+        form = ProfesorForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("profesor_list")
     else:
-        form = forms.ProfesorForm()
+        form = ProfesorForm()
     return render(request, "core/profesor_create.html", {"form": form})
 
 def estudiante_list(request):
-    consulta = models.Estudiante.objects.all()
+    consulta = Estudiante.objects.all()
     contexto = {"estudiantes": consulta}
     return render(request, "core/estudiante_list.html", contexto) 
 
 def estudiante_create(request):
     if request.method == "POST":
-        form = forms.EstudianteForm(request.POST)
+        form = EstudianteForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("estudiante_list")
     else:
-        form = forms.EstudianteForm()
+        form = EstudianteForm()
     return render(request, "core/estudiante_create.html", {"form": form})
+
+class CursoListView(ListView):
+    model = Curso
+    template_name = 'core/curso_list.html'  # Ajusta el nombre del template según tu estructura
+    context_object_name = 'cursos'
